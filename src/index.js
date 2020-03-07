c1fc947abb2d4e531157df69cb6fe8f0cee92893
Becca Saines <becca@streak.com>
2020-03-07 09:39:20 -0800

    gitignore

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import './styles.css';
 
 const MyComponent = () => (
-    <h1>Hello from My Component</h1>
+    <h1>Hello from My Super Duper Component</h1>
 );
 
 export default MyComponent;
