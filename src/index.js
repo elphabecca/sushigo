6e4e3100b1ef5abfcf65378fb374953f33b7470a
Becca Saines <becca@streak.com>
2020-03-07 11:27:24 -0800

    notes for followup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import React from 'react';
 import './styles.css';
 import Deck from './Deck';
 
+// TO DO:
+// Replace current cards w/ sushiGo cards
+// suffle button is broken (though I was going to re-do that anyway)
+// there's an incorrect # of cards - there's a "1" card in addition to the ace
+
 const MyComponent = () => (
     <h1><Deck /></h1>
 );
