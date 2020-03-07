5b9fdec0c95164446ad0a01cf8510889305b8f25
Becca Saines <becca@streak.com>
2020-03-07 10:39:43 -0800

    copying card deck example

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@
 import React from 'react';
 import './styles.css';
 
+import { Deck } from './Deck'
+import { Card } from './Card'
+
 const MyComponent = () => (
     <h1>Hello from My Super Duper Component</h1>
 );
