644fe9abedf0199a3467a08f4de7d0f007bc786e
Becca Saines <becca@streak.com>
2020-03-07 11:11:35 -0800

    copied deck of cards from elsewhere - loving that extra '1' card

--- examples/src/index.js
+++ examples/src/index.js
@@ -3,9 +3,11 @@
 import React from 'react';
 import { render} from 'react-dom';
 import MyComponent from '../../src';
+import Deck from '../../src';
+import Card from '../../src';
 
 const App = () => (
-    <MyComponent />
+    <div><Deck /></div>
 );
 
 render(<App />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@
 
 import React from 'react';
 import './styles.css';
-
-import { Deck } from './Deck'
-import { Card } from './Card'
+import Deck from './Deck';
 
 const MyComponent = () => (
-    <h1>Hello from My Super Duper Component</h1>
+    <h1><Deck /></h1>
 );
 
 export default MyComponent;
