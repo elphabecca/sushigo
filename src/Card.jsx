ea6cf8b7f65ce6c89870c4607cbef8d0f99711ac
Becca Saines <becca@streak.com>
2020-03-07 11:12:09 -0800

    Deck and Card

---
+++ src/Card.jsx
@@ -0,0 +1,16 @@
+/*** src/Card.js   ***/
+
+import React from 'react';
+import './styles.css';
+
+const Card = (props) => {
+  if (props.suit == "♣︎" || props.suit == "♠︎") {
+    return (<div className="card card-black"><div className="card-tl"><div className="card-value">{props.value}</div><div className="card-suit">{props.suit}</div></div><div className="card-br"><div className="card-value">{props.value}</div><div className="card-suit">{props.suit}</div></div></div>);
+  } else {
+    return (<div className="card card-red"><div className="card-tl"><div className="card-value">{props.value}</div><div className="card-suit">{props.suit}</div></div>
+      <div className="card-br"><div className="card-value">{props.value}</div><div className="card-suit">{props.suit}</div></div>
+      </div>);
+  }
+};
+
+export default Card;
